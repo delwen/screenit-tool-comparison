2ae90a3e831523e37cf344f22e2bc3fd90b025ce
Elisa <elisa.bascunan@gmail.com>
2023-11-21 12:11:41 +0100

    change tables for barplots

--- Visualization_test.R
+++ Visualization_test.R
@@ -1,4 +1,5 @@
 #### Visualization ScreenIT ####
+
 data <- read_csv(here("testing", "regset.csv"))
 
 ##### WORDCLOUDS ####
@@ -33,6 +34,35 @@ wordcloud(words = id_type$Var1,
 
 #### OTHER VISUALIZATIONS ####
 
+#LOCATION BARPLOT - BESSER
 
+# Select only the three columns of interest
+location <- data[, c("id_in_abstract", "id_in_methods", 
+                     "id_in_other_location")]
 
-          
+# Count TRUE values for each selected variable
+count_true <- colSums(location)
+
+#call plot.new()
+plot.new()
+
+#select custom colors
+custom_palette <- colorRampPalette(c("#4EA8DE", "#48BFE3", "#56CFE1"))
+
+#create barplot
+barplot(count_true, 
+        names.arg = colnames(location), 
+        col = custom_palette(length(count_true)), 
+        main = "General location of IDs", 
+        ylab = "Count",
+        ylim = c(0, max(count_true) + 15))
+
+# Calculate the midpoints of the bars
+bar_midpoints <- barplot(count_true, plot = FALSE)
+
+# Add total count labels on each column (centered)
+text(bar_midpoints, count_true, 
+     labels = count_true, 
+     pos = 3, 
+     col = "black", 
+     cex = 0.8)
